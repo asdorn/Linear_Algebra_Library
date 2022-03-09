7ac08f4951c2d0669ed87d9ad49f9e777b090370
Jebeta <asafdo14@gmail.com>
2022-03-09 02:30:37 +0200

    update

--- Test.py
+++ Test.py
@@ -3,7 +3,3 @@ A = [[1,0,0],[0,1,0],[0,0,1]]
 B = [[3,4],[6,7],[9,8]]
 A = Class_Matrices.Linear_Matrices(A)
 B = Class_Matrices.Linear_Matrices(B)
-M_A = A.create_block_matrix(B,"right")
-M_B = B.create_block_matrix(A,"right")
-print(M_A.row_vectors)
-print(M_A.is_block_matrix)
